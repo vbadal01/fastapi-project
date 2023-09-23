279d9d12466911142e9d44df2a1974aeb2761478
vbadal <vivek.kartikeya.badal.01@gmail.com>
2023-09-23 23:45:13 +0530

    Todo List Using FastAPI

---
+++ main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from models import Todo
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+todos = []
+
+
+# Get all todos
+@app.get("/todos")
+async def get_todos():
+    return {"todos": todos}
+
+
+# Get Single to do
+@app.get("/todos/{todo_id}")
+async def get_todos(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return {"todo": todo}
+    return {"message": "No todos found"}
+
+
+# Create a todo
+@app.post("/todos")
+async def get_todo(todo: Todo):
+    todos.append(todo)
+    return {"message": "Todo has been added"}
+
+
+# Update a todo
+@app.put("/todos/{todo_id}")
+async def update_todo(todo_id: int, todo_obj: Todo):
+    for todo in todos:
+        if todo.id == todo_id:
+            todo.id = todo_id
+            todo.item = todo_obj.item
+            return {"todo": todo}
+    return {"message": "No todos found to update"}
+
+
+# Delete a todo
+@app.delete("/todos/{todo_id}")
+async def delete_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            todos.remove(todo)
+            return {"message": "Todo has been Deleted"}
+    return {"message": "No todos found"}
